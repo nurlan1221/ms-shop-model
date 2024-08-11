666a974641a33a85324d186f3a1c4532ddba98ae
nurlan1221 <nbayramov592@gmail.com>
2024-08-11 09:00:43 -0700

    add not null validation

--- src/main/java/az/orient/msshopmodel/dto/ReqModel.java
+++ src/main/java/az/orient/msshopmodel/dto/ReqModel.java
@@ -1,10 +1,12 @@
 package az.orient.msshopmodel.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class ReqModel {
+    @NotNull(message = "Model name is required")
     private String name;
     private String description;
 }
